6f234ea830dc69a219001f1f77fd18f91db681dc
wanwan0622 <minnie20000622@yahoo.co.jp>
2020-08-26 22:44:29 +0900

    ある程度完成。残りは、表示をきれいにする

--- GUI約数列挙.py
+++ GUI約数列挙.py
@@ -1,5 +1,22 @@
 # -*- coding: utf-8 -*-
 
+def divisor(n): #n以下の約数列挙
+    i = 1
+    table = []
+    while i * i <= n:
+        if n % i == 0:
+            table.append(i)
+            table.append(n // i)
+        i += 1
+    table = list(set(table))
+    table.sort()
+    ans = ""
+    for j in table:
+        ans += str(j) + ' '
+    return ans
+
+
+
 #GUIを書いてみる
 
 import tkinter
@@ -13,24 +30,24 @@ root.title("divisor_Tkinter")
 ##ウィンドウの大きさを設定
 root.geometry("400x400")
 
+##ラベル作成
+label_question = tkinter.Label(root, text = "入力された数の約数を列挙します")
+label_question.grid(row = 0)
+
+##テキストボックスの作成
+txt = tkinter.Entry(width = 10)
+txt.place(x = 150, y = 30)
+
+##ボタンが押された時に実行される関数
+def clicked():
+    input_num = int(txt.get())
+    label_ans = tkinter.Label(root, text = divisor(input_num))
+    label_ans.grid(row = 5)
+    txt.delete(0, tkinter.END)
+
+##ボタンの作成
+button = tkinter.Button(root, text = "PUSH", command = clicked)
+button.place(x = 200, y = 30)
+
 ##イベントループ
 root.mainloop()
-
-
-
-
-def divisor(n): #n以下の約数列挙
-    i = 1
-    table = []
-    while i * i <= n:
-        if n % i == 0:
-            table.append(i)
-            table.append(n // i)
-        i += 1
-    table = list(set(table))
-    return table
-
-n = int(input())
-for i in divisor(n):
-    print(i)
-
