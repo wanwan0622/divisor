59a8564895544d5b79e52ca5eac743a0c9d74278
wanwan0622 <minnie20000622@yahoo.co.jp>
2020-08-26 23:05:28 +0900

    ボタンが表示されないバグと戦い中

--- GUI約数列挙.py
+++ GUI約数列挙.py
@@ -16,7 +16,6 @@ def divisor(n): #n以下の約数列挙
     return ans
 
 
-
 #GUIを書いてみる
 
 import tkinter
@@ -28,7 +27,7 @@ root = tkinter.Tk()
 root.title("divisor_Tkinter")
 
 ##ウィンドウの大きさを設定
-root.geometry("400x400")
+root.geometry("800x150")
 
 ##ラベル作成
 label_question = tkinter.Label(root, text = "入力された数の約数を列挙します")
@@ -36,18 +35,18 @@ label_question.grid(row = 0)
 
 ##テキストボックスの作成
 txt = tkinter.Entry(width = 10)
-txt.place(x = 150, y = 30)
+txt.place(x = 50, y = 30)
 
 ##ボタンが押された時に実行される関数
 def clicked():
     input_num = int(txt.get())
     label_ans = tkinter.Label(root, text = divisor(input_num))
-    label_ans.grid(row = 5)
+    label_ans.place(x = 10, y = 60)
     txt.delete(0, tkinter.END)
 
 ##ボタンの作成
 button = tkinter.Button(root, text = "PUSH", command = clicked)
-button.place(x = 200, y = 30)
+button.place(x = 150, y = 30)
 
 ##イベントループ
 root.mainloop()
